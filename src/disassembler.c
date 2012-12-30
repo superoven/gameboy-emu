68c4414e6140db34305345caf84335153ad13232
Donovan McKelvey <programmingmind@gmail.com>
2012-12-29 17:23:39 -0800

    even more opcodes

--- src/disassembler.c
+++ src/disassembler.c
@@ -100,10 +100,64 @@ int disassemble(uint16_t address)
     return 1;
   }
   
+  if(B1_07==0x2A)
+  {
+    printf("H <- (nn+1) , L <- (nn)\n");
+    return 3;
+  }
+  
+  if(B1_07==0x22)
+  {
+    printf("(nn+1) <- H, (nn) <- L\n");
+    return 3;
+  }
+  
+  if(B1_07==0xF9)
+  {
+    printf("LD SP <- HL\n");
+    return 1;
+  }
+  
+  if(B1_07==0xEB)
+  {
+    printf("EX DE <-> HL\n");
+    return 1;
+  }
+  
+  if(B1_07==0x08)
+  {
+    printf("EX AF <-> AF'\n");
+    return 1;
+  }
+  
+  if(B1_07==0xD9)
+  {
+    printf("EXX (BC)<->(BC'),(DE)<->(DE'),(HL)<->(HL')\n");
+    return 1;
+  }
+  
+  if(B1_07==0xE3)
+  {
+    printf("EX H<->(SP+1),L<->(SP)\n");
+    return 1;
+  }
+  
   if(B1_01==0 && B1_47==1)
   {
     printf("LD dd <- nn\n");
     return 3;
+  }
+  
+  if(B1_01==3 && B1_47==5)
+  {
+    printf("PUSH (SP-2) <- qqL, (SP-1) <- qqH\n");
+    return 1;
+  }
+  
+  if(B1_01==3 && B1_47==1)
+  {
+    printf("POP qqH <- (SP+1), qqL <- (SP)\n");
+    return 1;
   }
   
   if(B1_01==0 && B1_57==6)
@@ -148,16 +202,70 @@ int disassemble(uint16_t address)
     return 4;
   }
   
-  if(B1_07==0xDD && B2_01==1 && B2_24==6)
+  if(B1_07==0xDD && B2_07==0x2A)
   {
-    printf("LD (IX+d) <- r\n");
-    return 3;
+    printf("IXh <- (nn+1), IXI <- (nn)\n");
+    return 4;
   }
   
-  if(B1_07==0xDD && B2_01==1 && B2_57==6)
+  if(B1_07==0xDD && B2_07==0x22)
   {
-    printf("LD r <- (IX+d)\n");
-    return 3;
+    printf("(nn+1) <- IXh, (nn) <- IXI\n");
+    return 4;
+  }
+  
+  if(B1_07==0xDD && B2_07=0xF9)
+  {
+    printf("LD SP <- -IX\n");
+    return 2;
+  }
+  
+  if(B1_07==0xDD && B2_07==0xE5)
+  {
+    printf("PUSH (SP-2) <- IXL, (SP-1) <- IXH\n");
+    return 2;
+  }
+  
+  if(B1_07==0xDD && B2_07==0xE1)
+  {
+    printf("POP IXH <- (SP+1), IXL <- (SP)\n");
+    return 2;
+  }
+  
+  if(B1_07==0xDD && B2_07==0xE3)
+  {
+    printf("EX IXH<->(SP+1),IXL<->(SP)\n");
+    return 1;
+  }
+  
+  if(B1_07==0xDD && B2_07==0xFD)
+  {
+    printf("POP IYH <- (SP-X1), IYL <- (SP)\n");
+    return 2;
+  }
+  
+  if(B1_07==0xED && B2_07==0xA0)
+  {
+    printf("LDI (DE)<-(HL),DE <- DE+1, HL <- HL+1, BC <- BC-1\n");
+    return 2;
+  }
+  
+  if(B1_07==0xED && B2_07==0xA8)
+  {
+    printf("LDD (DE)<-(HL),DE <- DE-1,HL <- HL-1,BC <- BC-1\n");
+    return 2;
+  }
+  
+  if(B1_07==0xED && B2_07==0xB8)
+  {
+    printf("LDDR (DE)<-(HL),DE <- D <- 1,HL <- HL-1,BC <- BC-1\n");
+    return 2;
+  }
+  
+  if(B1_07==0xED && B2_07==0xB0)
+  {
+    printf("LDIR (DE)<-(HL),DE <- DE+1, HL <- HL+1, BC F <-> BC-1\n");
+    return 2;
   }
   
   if(B1_07==0xFD && B2_07==0x36)
@@ -170,6 +278,60 @@ int disassemble(uint16_t address)
   {
     printf("LD IY <- nn\n");
     return 4;
+  }
+  
+  if(B1_07==0xFD && B2_07==0x22)
+  {
+    printf("(nn+1) <- IYh, (nn) <- IYI\n");
+    return 4;
+  }
+  
+  if(B1_07==0xFD && B2_07==0x2A)
+  {
+    printf("IYh <- (nn+1), IYI <- (nn)\n");
+    return 4;
+  }
+  
+  if(B1_07==0xFD && B2_07==0xE3)
+  {
+    printf("EX IYH<->(SP+1),IYL<->(SP)\n");
+    return 2;
+  }
+  
+  if(B1_07==0xFD && B2_07==0xE5)
+  {
+    printf("PUSH (SP-2) <- IYL, (SP-1) <- IYH\n");
+    return 2;
+  }
+  
+  if(B1_07==0xFD && B2_07==0xF9)
+  {
+    printf("LD SP <-IY\n");
+    return 2;
+  }
+  
+  if(B1_07==0xED && B2_01==1 && B2_47==0xB)
+  {
+    printf("ddh <- (nn+1) ddl <- (nn)\n");
+    return 4;
+  }
+  
+  if(B1_07==0xED && B2_01==1 && B2_47==3)
+  {
+    printf("(nn+1) <- ddh, (nn) <- ddl\n");
+    return 4;
+  }
+  
+  if(B1_07==0xDD && B2_01==1 && B2_24==6)
+  {
+    printf("LD (IX+d) <- r\n");
+    return 3;
+  }
+  
+  if(B1_07==0xDD && B2_01==1 && B2_57==6)
+  {
+    printf("LD r <- (IX+d)\n");
+    return 3;
   }
   
   if(B1_07==0xFD && B2_01==1 && B2_24==6)
